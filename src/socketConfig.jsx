0b799f927cd733afa93fd2649f89d8b70a44a9bd
mdxaasil <mdaasil18@gmail.com>
2023-05-29 00:34:58 +0530

    integrate with backend

--- src/socketConfig.jsx
+++ src/socketConfig.jsx
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
-const socket = io('http://localhost:3001');
-// const socket = io('https://localhost:3001', { transports : ['websocket'] });
+// const socket = io('http://localhost:3001');
+const socket = io('https://type-racer-backend.onrender.com');
+
 export default socket
