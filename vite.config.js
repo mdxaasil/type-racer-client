ba20f651de8e31e527e28a53ad626eb71f856cca
mdxaasil <mdaasil18@gmail.com>
2023-05-28 23:56:30 +0530

    test 1

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
   // server: {
   //   port: 3000
   // },
-  base: "/type-racer-client/"
+  // base: "/type-racer-client/"
 
 });
