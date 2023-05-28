7e42ec038a768a574411b23350228503a5273e0f
mdxaasil <mdaasil18@gmail.com>
2023-05-28 23:34:28 +0530

    workflow update

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
+  // server: {
+  //   port: 3000
+  // },
+  base: "/type-racer-client/"
 
 });
